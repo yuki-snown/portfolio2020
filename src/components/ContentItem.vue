aa2808cb6eeac0bf0cdeda309ee437269832d96c
yuki-snown <yuki.snowman.0103@icloud.com>
2020-05-14 10:21:19 +0900

    page1 追加

---
+++ src/components/ContentItem.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="contentitem">
+        <h1>{{explain}}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ProfileItem',
+    props: {
+        explain: String
+    }
+}
+</script>
---
+++ src/views/Page1.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="page1">
+        <ContentItem msg="Page1"/>
+    </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import ContentItem from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'ContentItem',
+  components: {
+    ContentItem
+  }
+}
+</script>
